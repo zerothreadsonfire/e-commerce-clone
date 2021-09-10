f36eba2ddd6b5aa92375842b9e2ec4afe9193450
satyam <satyamonsocial@gmail.com>
2021-09-10 20:41:33 +0530

    implement product screen page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import { Row, Col, Button, Image, ListGroup, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductScreen = () => {
-  return(
-    <div>
-      hello world
-    </div>
+const ProductScreen = ({ match }) => {
+  const product = products.find(p => p._id === match.params.id);
+
+  return (
+    <React.Fragment>
+      <Button as={Link} to="/" className="btn btn-light my-3">Go Back</Button>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item><h3>{product.name}</h3></ListGroup.Item>
+            <ListGroup.Item><Rating value={product.rating} text={`${product.numReviews} reviews`} /></ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col><strong>${product.price}</strong></Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button type="button" className="btn btn-block" disabled={product.countInStock === 0}>Add To Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </React.Fragment>
   );
 }
 
