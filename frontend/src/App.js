3a3cdc248dc6bdfaf2c551e4a529fd8545599e8a
satyam <satyamonsocial@gmail.com>
2021-09-10 19:30:44 +0530

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import './bootstrap.min.css';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductScreen = () => {
+  return(
+    <div>
+      hello world
+    </div>
+  );
+}
+
+export default ProductScreen;
