0df4821b6e1059524f72f334a5296bd04f42d1c4
satyam <satyamonsocial@gmail.com>
2021-09-10 12:25:55 +0530

    add home screen product listing

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from '../products';
+import Product from '../components/Product';
+
+const HomeScreen = () => {
+  return <React.Fragment>
+    <h1>Latest Products</h1>
+    <Row>
+      {
+        products.map(product => {
+          return <Col key={product._id} xl={3} lg={4} md={6} sm={12}>
+            <Product product={product}/>
+          </Col>
+        })
+      }
+    </Row>
+  </React.Fragment>
+}
+
+export default HomeScreen;
