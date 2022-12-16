60845cb483de16064345e6741dcd082a766f4480
satyamKumar <satyamkumar2302@gmail.com>
2022-12-16 17:15:27 +0530

    server to serve frontend static build

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -22,10 +23,6 @@ if(process.env.NODE_ENV === 'Development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('welcome to backend!');
-})
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -33,7 +30,17 @@ app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-const __dirname = path.resolve();
+if(process.env.NODE_ENV === "production") {
+  app.use("", express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}else {
+  app.get("/", (req, res) => {
+    res.send("Server is running in development mode")
+  })
+}
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
